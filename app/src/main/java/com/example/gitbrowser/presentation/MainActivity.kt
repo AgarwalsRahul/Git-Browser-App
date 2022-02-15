d079c82e96116084869517b371615899fb2ee52e
AgarwalsRahul <rahul.agarwal2019@vitstudent.ac.in>
2022-02-15 18:14:28 +0530

    Added Fragments and Navigation graph

--- app/src/main/java/com/example/gitbrowser/presentation/MainActivity.kt
+++ app/src/main/java/com/example/gitbrowser/presentation/MainActivity.kt
@@ -2,11 +2,17 @@ package com.example.gitbrowser.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
 import com.example.gitbrowser.R
+import com.example.gitbrowser.databinding.ActivityMainBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
