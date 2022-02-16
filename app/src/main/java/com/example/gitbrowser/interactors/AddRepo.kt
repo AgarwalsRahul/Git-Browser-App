8fc2a96083888dfc8132b835c9086dca97bbadbf
AgarwalsRahul <rahul.agarwal2019@vitstudent.ac.in>
2022-02-16 11:20:52 +0530

    Set up interactors

--- app/src/main/java/com/example/gitbrowser/interactors/AddRepo.kt
+++ app/src/main/java/com/example/gitbrowser/interactors/AddRepo.kt
@@ -1,4 +1,42 @@
 package com.example.gitbrowser.interactors
 
-class AddRepo {
+import com.example.gitbrowser.dataSource.cache.RepoDao
+import com.example.gitbrowser.dataSource.cache.model.RepoEntityMapper
+import com.example.gitbrowser.dataSource.network.RepoService
+import com.example.gitbrowser.dataSource.network.response.RepoDtoMapper
+import com.example.gitbrowser.domain.data.DataState
+import com.example.gitbrowser.domain.model.Repo
+import kotlinx.coroutines.flow.flow
+import java.lang.Exception
+
+class AddRepo(
+    private val repoDao: RepoDao,
+    private val entityMapper: RepoEntityMapper,
+    private val repoService: RepoService,
+    private val repoDtoMapper: RepoDtoMapper
+) {
+
+    fun execute(owner: String, repo: String) = flow<DataState<Repo>> {
+        try {
+            emit(DataState.loading())
+            // get repo from network
+            val networkRepo = getRepoFromNetwork(owner, repo)
+            // insert it into cache
+            val result = repoDao.addRepo(entityMapper.mapFromDomainModel(networkRepo))
+            if(result<0){
+                throw Exception("Unable to add repo. Please try again")
+            }else{
+                emit(DataState.success(networkRepo))
+            }
+
+        } catch (e: Exception) {
+            emit(DataState.error<Repo>(e.message ?: "Unknown Error"))
+        }
+
+    }
+
+
+    private suspend fun getRepoFromNetwork(owner: String, repo: String): Repo {
+        return repoDtoMapper.mapToDomainModel(repoService.getRepo(owner, repo))
+    }
 }
--- app/src/main/java/com/example/gitbrowser/interactors/GetRepo.kt
+++ app/src/main/java/com/example/gitbrowser/interactors/GetRepo.kt
@@ -1,4 +1,30 @@
 package com.example.gitbrowser.interactors
 
-class GetRepo {
+import com.example.gitbrowser.dataSource.cache.RepoDao
+import com.example.gitbrowser.dataSource.cache.model.RepoEntityMapper
+import com.example.gitbrowser.domain.data.DataState
+import com.example.gitbrowser.domain.model.Repo
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.flow
+import java.lang.Exception
+
+class GetRepo(
+    private val entityMapper: RepoEntityMapper,
+    private val repoDao: RepoDao
+) {
+    fun execute(page: Int): Flow<DataState<List<Repo>>> = flow {
+        try {
+            emit(DataState.loading())
+            repoDao.getRepos(page).collect {
+                emit(
+                    DataState.success(
+                        entityMapper.fromEntityList(it)
+                    )
+                )
+            }
+        } catch (e: Exception) {
+            emit(DataState.error(e.message ?: "Unknown Error"))
+        }
+    }
 }
